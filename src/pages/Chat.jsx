2ccb463bbaf7889e0523ba15f85d1d2a2fb5d459
dasom222g <dasom228@gmail.com>
2026-07-03 14:44:33 +0900

    feat: /message API추가

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -5,27 +5,69 @@ import { MoonLoader } from "react-spinners";
 
 const Chat = ({ ingredientList }) => {
   // logic
-  console.log("🚀~ ingredientList:", ingredientList);
   const endpoint = process.env.REACT_APP_SERVER_ADDRESS;
-  console.log("🚀 ~ endpoint:", endpoint);
 
   const [value, setValue] = useState("");
 
   // TODO: set함수 추가하기
-  const [messages, setMessages] = useState([]); // chatGPT와 사용자의 대화 메시지 배열
   const [infoMessages, setInfoMessages] = useState([]); // 초기세팅 메시지
+  const [messages, setMessages] = useState([]); // chatGPT와 사용자의 대화 메시지 배열
 
-  const [isInfoLoading] = useState(false); // 최초 정보 요청시 로딩
-  const [isMessageLoading] = useState(true); // 사용자와 메시지 주고 받을때 로딩
+  const [isInfoLoading, setIsInfoLoading] = useState(true); // 최초 정보 요청시 로딩
+  const [isMessageLoading, setIsMessageLoading] = useState(false); // 사용자와 메시지 주고 받을때 로딩
   const hadleChange = (event) => {
     const { value } = event.target;
-    console.log("value==>", value);
     setValue(value);
+  };
+
+  const sendMessage = async (userMessage, allMessages) => {
+    // "/messge"API 호출 및 관련 state업데이트
+    setIsMessageLoading(true);
+    try {
+      const response = await fetch(`${endpoint}/message`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userMessage, messages: allMessages }),
+      });
+
+      const result = await response.json();
+      console.log("🚀 ~ result:", result);
+
+      // 응답받은 답변을 대화 목록에 추가
+      setMessages((prev) => [
+        ...prev,
+        {
+          role: "assistant",
+          content: result.data.content,
+        },
+      ]);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsMessageLoading(false);
+    }
   };
 
   const hadleSubmit = (event) => {
     event.preventDefault();
     console.log("메시지 보내기");
+    // 1. userMessage (현재 사용자가 입력한 메시지 정보)
+    const userMessage = {
+      role: "user",
+      content: value.trim(),
+    };
+    // 2. messages (기존 대화 목록)
+    const allMessages = [...infoMessages, ...messages];
+
+    // State관리
+    // prev: 기존 대화 목록 (배열)
+    setMessages((prev) => [...prev, userMessage]);
+
+    // 메시지 초기화
+    setValue("");
+
+    // api 호출
+    sendMessage(userMessage, allMessages);
   };
 
   // 초기 세팅
@@ -41,7 +83,7 @@ const Chat = ({ ingredientList }) => {
 
       // JSON -> 자바스크립 객체 변환
       const result = await response.json();
-      console.log("🚀 ~ result:", result);
+      // console.log("🚀 ~ result:", result);
 
       // 데이터가 잘 들어오지 않은경우는 뒷코드 실행안함
       if (!result.data) return;
@@ -61,6 +103,9 @@ const Chat = ({ ingredientList }) => {
       setMessages((prev) => [...prev, { role, content }]);
     } catch (error) {
       console.error(error);
+    } finally {
+      // API응답 완료 후 실행
+      setIsInfoLoading(false);
     }
   };
 
@@ -70,10 +115,6 @@ const Chat = ({ ingredientList }) => {
     sendInfo(ingredientList);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  useEffect(() => {
-    console.log("messages", messages);
-  }, [messages]);
 
   // view
   return (
